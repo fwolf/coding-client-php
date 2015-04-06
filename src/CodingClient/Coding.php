342d73b9964285fb5ee2e809d6d01291858b5e3e
Fwolf <fwolf.aide+git@gmail.com>
2015-04-06 18:03:56 +0800

    New Coding class, the main entrance

---
+++ src/CodingClient/Coding.php
@@ -0,0 +1,19 @@
+<?php
+namespace Fwolf\Client\Coding;
+
+use Fwolf\Client\Coding\Helper\AuthenticationTrait;
+use Fwolf\Client\Coding\Helper\CurlAwareTrait;
+
+/**
+ * Coding
+ *
+ * Main entrance.
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+class Coding
+{
+    use AuthenticationTrait;
+    use CurlAwareTrait;
+}
