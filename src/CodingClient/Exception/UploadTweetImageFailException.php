f84c0fa4aecbb4e6c0b352a9aeb30e71b9b08bd7
Fwolf <fwolf.aide+git@gmail.com>
2015-04-07 17:11:07 +0800

    Implement tweet with image feature

---
+++ src/CodingClient/Exception/UploadTweetImageFailException.php
@@ -0,0 +1,10 @@
+<?php
+namespace Fwolf\Client\Coding\Exception;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+class UploadTweetImageFailException extends \Exception
+{
+}
--- src/CodingClient/Helper/TweetTrait.php
+++ src/CodingClient/Helper/TweetTrait.php
@@ -2,6 +2,7 @@
 namespace Fwolf\Client\Coding\Helper;
 
 use Fwolf\Client\Coding\Exception\SendTweetFailException;
+use Fwolf\Client\Coding\Exception\UploadTweetImageFailException;
 
 /**
  * TweetTrait
@@ -19,25 +20,58 @@ trait TweetTrait
      * Send a tweet
      *
      * @param   string  $content
+     * @param   array   $images     {description: filePath}
      * @return  array   New added tweet data
      * @throws  SendTweetFailException
      */
-    public function sendTweet($content)
+    public function sendTweet($content, $images = [])
     {
         $this->login();
 
+        foreach ($images as $key => $image) {
+            $imageUrl = $this->uploadImage($image);
+
+            $key = is_int($key) ? "图片{$key}" : $key;
+
+            $content .= "\n![$key]($imageUrl)";
+        }
+
         $curl = $this->getCurl();
+        $content = urlencode($content);
+        $device = urlencode($this->getDevice());
         $result = $curl->post(
-            'tweet',
-            [
-                'content' => $content,
-                'device'  => $this->getDevice(),
-            ]
+            "tweet?content={$content}&device={$device}",
+            []
         );
         $resultArray = json_decode($result, true);
 
         if (empty($result) || 0 != $resultArray['code']) {
             throw new SendTweetFailException($result);
+        }
+
+        return $resultArray['data'];
+    }
+
+
+    /**
+     * Upload image, got stored url
+     *
+     * @param   string  $image  File path of image
+     * @return  string
+     */
+    public function uploadImage($image)
+    {
+        $curl = $this->getCurl();
+        $result = $curl->post(
+            'tweet/insert_image',
+            [
+                'tweetImg' => new \CURLFile($image),
+            ]
+        );
+        $resultArray = json_decode($result, true);
+
+        if (empty($result) || 0 != $resultArray['code']) {
+            throw new UploadTweetImageFailException($result);
         }
 
         return $resultArray['data'];
