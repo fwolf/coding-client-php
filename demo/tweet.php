9cdc94acf4afe80898cc3df5f73dce7cd6bd212b
Fwolf <fwolf.aide+git@gmail.com>
2015-04-07 15:30:24 +0800

    Add comment about why renew Coding instance

--- demo/tweet.php
+++ demo/tweet.php
@@ -20,10 +20,20 @@ EOF;
             ->login();
 
         // Reload cookie
-        // Question: unsetCurl() not work, only total new Coding instance work
-        $client = (new Coding())
-            ->setAuthentication($argv[1], $argv[2])
-            ->setCookieFile($cookieFile);
+        $client = new Coding();
+        $client->setCookieFile($cookieFile)
+            ->setAuthentication($argv[1], $argv[2]);
+
+        /**
+         * Question: unsetCurl() not work, only total new Coding instance
+         * work, The curl log shown request header include 'Cookie: sid=...',
+         * but server response as not logged in.
+         *
+         * The recreation of Coding instance works, but need a duplicate
+         * login() action, and re-assign all attributes.
+         */
+//        $client->unsetCurl()
+//            ->setCookieFile($cookieFile);
 
         $tweet = $client->sendTweet($argv[3]);
         echo "Tweet {$tweet['id']} sent" . PHP_EOL;
